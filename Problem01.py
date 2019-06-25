cd1b8d83b63f2b0799e1a35f0419be1104e61a00
ShreyaRawal97 <50499340+ShreyaRawal97@users.noreply.github.com>
2019-06-25 11:51:13 -0500

    Add files via upload

---
+++ Problem01.py
@@ -0,0 +1,19 @@
+"""
+This problem was recently asked by Google.
+
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
+"""
+
+k = 14
+list = [10, 15, 3, 7, 7]
+
+s = set(list)
+def sum_exists(list, k):
+    return bool({k - i for i in list} & s)
+
+print(sum_exists(list, k))
+#the run time of this program is O(n)
